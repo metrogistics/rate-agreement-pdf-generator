d9128f3fdabdaf70dcabd303827943e151410fb6
Oliver Kelso <oliver@pixelsoul.com>
2021-03-12 17:06:22 -0600

    Embedded fonts in data uri because they were not rendering on PDF. Adjusted font sizes for PDF rendering. Added handlebar template tags to html for PDF rendering.  Updated the .gitignore to be correct. Deleted the dist directory.

--- dist/middleware/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logger = void 0;
-const winston_1 = require("./winston");
-Object.defineProperty(exports, "logger", { enumerable: true, get: function () { return winston_1.logger; } });
-//# sourceMappingURL=index.js.map
